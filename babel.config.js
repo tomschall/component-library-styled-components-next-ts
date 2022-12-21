7f6a76dad390a268c5acf551ee87ffcbaa36ed8f
Thomas Schallert <thomas.schallert@fhnw.ch>
2022-12-21 22:40:09 +0100

    fix babel config

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = {
   presets: ['next/babel', '@babel/preset-react', '@babel/preset-typescript'],
   plugins: [
     'babel-plugin-macros',
-    [('babel-plugin-styled-components', { ssr: true })],
+    ['babel-plugin-styled-components', { ssr: true }],
   ],
 };
